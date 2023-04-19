acf68319c9b80998f34475470c394ea427e41eab
mounaskander <skander.mouna@hotmail.com>
2023-04-19 14:12:34 +0200

    saved data o csv file

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ async function start() {
         // await browser.close()
     } catch (e) {
         console.log(e)
-        await browser.close()
+        //await browser.close()
     }
 }
 
@@ -23,12 +23,16 @@ async function launching() {
     await page.goto("https://sport.aiv.hfoed.de/ords/f?p=41666:4:");
     await page.waitForSelector('div', {timeout: 5000});
 
-    await page.screenshot({path: "img.png"})
+  //  await page.screenshot({path: "img.png"})
 
 }
 
 async function getInformation() {
     await getDayInformation("Monday", "#report_R12610172541656757722")
+    console.log("hehe")
+    await getDayInformation("Tuesday", "#report_R12610186338540106932")
+    console.log("hehe")
+    await getDayInformation("Wensday", "#report_R12610187325857112688")
 
 }
 
@@ -41,7 +45,7 @@ async function getDayInformation(day, dayID) {
 let columnData=[]
     let index=0
     const getNumberOfPlans = await page.$$eval(selector, trs => trs.length);
-    for (let i = 2; i < getNumberOfPlans; i++) {
+    for (let i = 2; i <= getNumberOfPlans; i++) {
         rowSelector = selector + ':nth-child(' + i.toString() + ')'
         columnSelector = rowSelector + '>td'
      columnData[index]=   await getColumnInformation(columnSelector)
@@ -53,7 +57,11 @@ let columnData=[]
         index++
 
     }
-    console.log(columnData.toString()+"\n")
+    setCSVFile(columnData)
+    for (let i = 0; i <columnData.length ; i++) {
+        console.log(columnData[i].start)
+
+    }
 }
 
 async function getColumnInformation(columnSelector) {
@@ -69,7 +77,27 @@ async function getColumnInformation(columnSelector) {
         data[index] = getInformation
         index++
     }
-    return data
+    //transform data table to an object
+    let dataObj={start:data[0],
+    end:data[1],
+    art:data[2],
+    ort:data[3],
+    platz:data[4],
+    beschreibung:0}
+    return dataObj
 }
+/**
+ * convert table of objects  (products) into csv file
+ * make sure that there is no excessive comma
+ * @param products
+ * @returns {Promise<void>}
+ */
+async function setCSVFile(products) {
 
+    const ObjectsToCsv = require('objects-to-csv')
+
+    const csv = new ObjectsToCsv(products)
+
+    await csv.toDisk('./plan.csv', {append: true})
+}
 start()
