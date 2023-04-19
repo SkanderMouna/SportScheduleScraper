a6481f9b09050ec529978e4804429034103d8ffe
mounaskander <skander.mouna@hotmail.com>
2023-04-19 13:37:51 +0200

    backup

--- index.js
+++ index.js
@@ -37,13 +37,23 @@ async function getDayInformation(day, dayID) {
     let selector = dayID + ' > tbody > tr:nth-child(2) > td > table > tbody > tr'
     let rowSelector
     let columnSelector
-    let getNumberOfPlans = await page.$$(selector)
-    for (let i = 2; i < getNumberOfPlans.length; i++) {
+   // let getNumberOfPlans = await page.$$(selector)
+let columnData=[]
+    let index=0
+    const getNumberOfPlans = await page.$$eval(selector, trs => trs.length);
+    for (let i = 2; i < getNumberOfPlans; i++) {
         rowSelector = selector + ':nth-child(' + i.toString() + ')'
         columnSelector = rowSelector + '>td'
-        await getColumnInformation(columnSelector)
+     columnData[index]=   await getColumnInformation(columnSelector)
+        //todo for everycolmn add to json file
+        /*
+        {"monday":[{begin:18:00,ende:19:00},{begin:19:00,ende:20:00}]
+         */
+
+        index++
 
     }
+    console.log(columnData.toString()+"\n")
 }
 
 async function getColumnInformation(columnSelector) {
@@ -59,7 +69,7 @@ async function getColumnInformation(columnSelector) {
         data[index] = getInformation
         index++
     }
-    console.log(data.toString())
+    return data
 }
 
 start()
